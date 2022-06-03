e37ab0f0adee0c789edfab7e506938f468c74e98
Ajmal Muhammed <ajmalmuhammed@Ajmals-MacBook-Air.local>
2022-06-03 11:57:30 +0530

    cart id field changed

--- store/models.py
+++ store/models.py
@@ -123,7 +123,7 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid4, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
