368d8361d04eac9c23246f88d3c3f5cf1644a01c
Ajmal Muhammed <ajmalmuhammed@Ajmals-MacBook-Air.local>
2022-06-03 11:34:45 +0530

    initial update

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -175,23 +175,11 @@ ADMINS = [
 ]
 
 #redis message broker for celery
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BEAT_SCHEDULE = {
     'notify_customers': {
         'task': 'playground.tasks.notify_customers',
         'schedule': 10,
         'args': ['Hello Ajmal']
-    }
-}
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
-        'TIMEOUT': 10 * 60,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
     }
 }
 
--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import *
 
 
@@ -6,4 +7,21 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['virtualecomm-prod.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        'TIMEOUT': 10 * 60,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
