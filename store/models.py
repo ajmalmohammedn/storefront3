f7f81ea7b50f95b70245d64d7dce29c60f632515
Ajmal Muhammed <ajmalmuhammed@Ajmals-MacBook-Air.local>
2022-06-03 17:18:27 +0530

    migrations of store deleted

--- store/models.py
+++ store/models.py
@@ -123,7 +123,7 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, unique=True)
+    id = models.UUIDField(primary_key=True,default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
